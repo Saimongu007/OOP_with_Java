b916b47f66bbbfa94eb8a5e16d6847b612806e8c
Saimongu Marma <124234753+Saimongu007@users.noreply.github.com>
2023-10-16 01:00:22 +0600

    Basic

---
+++ Datatype.java
@@ -0,0 +1,23 @@
+public class Datatype {
+    public static void main(String[] args)
+    {
+        int num1;
+        double num2;
+        float num3;
+        String firstname;
+        String lastname;
+
+        num1 = 1;
+        num2 = 2.5;
+        num3 = 3.6f;
+        firstname = "Saimongu";
+        lastname = "Marma";
+
+        System.out.println(num1);
+        System.out.println(num2);
+        System.out.println(num3);
+        System.out.println(firstname);
+        System.out.println(lastname);
+        System.out.println(firstname+" "+lastname);
+    }
+}
